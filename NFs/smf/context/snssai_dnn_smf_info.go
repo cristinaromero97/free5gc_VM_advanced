650cde3b5ba1edf1b90122ace3496075ddcb9820
cristinaromero97 <cristina.romerofr@gmail.com>
2021-05-19 07:49:45 +0000

    Initial commit

---
+++ NFs/smf/context/snssai_dnn_smf_info.go
@@ -0,0 +1,19 @@
+package context
+
+import "net"
+
+// SnssaiSmfInfo records the SMF S-NSSAI related information
+type SnssaiSmfInfo struct {
+	Snssai   SNssai
+	DnnInfos map[string]*SnssaiSmfDnnInfo
+}
+
+// SnssaiSmfDnnInfo records the SMF per S-NSSAI DNN information
+type SnssaiSmfDnnInfo struct {
+	DNS DNS
+}
+
+type DNS struct {
+	IPv4Addr net.IP
+	IPv6Addr net.IP
+}
